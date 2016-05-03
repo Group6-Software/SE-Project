46ad7c9262e34fe71294323411b367607118fb9b
mhaseebmlk <17100254@lums.edu.pk>
2016-05-03 10:41:47 +0000

    getting employee login

--- db/migrate/20160503102901_fixing_items_cost.rb
+++ db/migrate/20160503102901_fixing_items_cost.rb
@@ -1,11 +1,11 @@
-class FixingItemsCost < ActiveRecord::Migration
-  # def change
-  # end
-  def up
-    change_column :items, :cost, 'integer USING CAST(cost AS integer)'
-  end
+# class FixingItemsCost < ActiveRecord::Migration
+#   # def change
+#   # end
+#   def up
+#     change_column :items, :cost, 'integer USING CAST(cost AS integer)'
+#   end
   
-  # def down
-  #   change_column :profiles, :show_attribute, :boolean, :default => nil
-  # end
-end
+#   # def down
+#   #   change_column :profiles, :show_attribute, :boolean, :default => nil
+#   # end
+# end
